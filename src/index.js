400b89c5788571276a6d07a0281eb6777dace23f
zeb <zebbey@gmail.com>
2024-08-12 14:29:53 +0800

    my domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.zebplay.com": dockerHub,
+  "quay.zebplay.com": "https://quay.io",
+  "gcr.zebplay.com": "https://gcr.io",
+  "k8s-gcr.zebplay.com": "https://k8s.gcr.io",
+  "k8s.zebplay.com": "https://registry.k8s.io",
+  "ghcr.zebplay.com": "https://ghcr.io",
+  "cloudsmith.zebplay.com": "https://docker.cloudsmith.io",
+  "ecr.zebplay.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.zebplay.com": dockerHub,
 };
 
 function routeByHosts(host) {
